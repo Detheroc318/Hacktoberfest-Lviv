1a5eedb36fd0f7a2a27a3a8f975dce0b4e8421ba
DrevychLiubomyr <ldrevych@gmail.com>
2019-10-12 13:55:50 +0300

    [+] Add helpers for all project which contain(assets, color and string)

---
+++ Hacktoberfest/Hacktoberfest/Helpers/AppColor.swift
@@ -0,0 +1,18 @@
+//
+//  AppColor.swift
+//  Hacktoberfest
+//
+//  Created by Luybckyk Drevych on 12.10.2019.
+//  Copyright © 2019 Luybckyk Drevych. All rights reserved.
+//
+
+import UIKit
+
+extension UIColor {
+    
+    static var mainGreen: UIColor {
+        return UIColor(red: 80, green: 227, blue: 194, alpha: 1)
+    }
+
+
+}
