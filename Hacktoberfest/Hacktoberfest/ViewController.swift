81cb88c0c7418e5ceb0c488a87994926fb31ea1b
DrevychLiubomyr <ldrevych@gmail.com>
2019-10-12 12:12:23 +0300

    [+] Add Started Project

---
+++ Hacktoberfest/Hacktoberfest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Hacktoberfest
+//
+//  Created by Luybckyk Drevych on 12.10.2019.
+//  Copyright © 2019 Luybckyk Drevych. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
