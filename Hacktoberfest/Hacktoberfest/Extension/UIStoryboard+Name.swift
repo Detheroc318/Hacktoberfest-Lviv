28828acbed93be52d37105c6ec0d47e4771fec68
DrevychLiubomyr <ldrevych@gmail.com>
2019-10-12 13:04:42 +0300

    [+] Add extension to UIStoryboard to get ViewController

---
+++ Hacktoberfest/Hacktoberfest/Extension/UIStoryboard+Name.swift
@@ -0,0 +1,26 @@
+//
+//  UIStoryboard+Name.swift
+//  Zupa
+//
+//  Created by binariksuser1111 on 9/27/19.
+//  Copyright © 2019 binariksuser1111. All rights reserved.
+//
+
+import UIKit
+
+extension UIStoryboard {
+    
+    public enum Names {
+        static let Main = UIStoryboard(name: "Main", bundle: nil)
+        static let SingUp = UIStoryboard(name: "SingUp", bundle: nil)
+    }
+    
+    enum Main {
+        static var welcomeVC: WelcomeViewController  {
+            return Names.Main.initialViewController(with: WelcomeViewController.self)
+        }
+    }
+    
+ 
+    
+}
